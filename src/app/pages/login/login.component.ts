e434c2991b877942e138dbf9df84426bea076536
unkv0wn <cjeanb0@gmail.com>
2025-05-23 22:02:19 -0300

    ✨ Criando tela de login e cadastro de novos usuarios
    Criado tela de login e cadastro de novos usuarios ja funcionando  restando apenas o backend

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { Eye, EyeClosed, LucideAngularModule } from 'lucide-angular';
+import { LogoComponent } from '../../components/logo/logo.component';
+
+@Component({
+  selector: 'LoginPage',
+  imports: [CommonModule, LucideAngularModule, LogoComponent, FormsModule],
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  readonly EyeOpen = Eye;
+  readonly EyeClosed = EyeClosed;
+
+  isLogin = false;
+  isCNPJ = false;
+  isPasswordVisible: boolean = true;
+  username: string = '';
+  password: string = '';
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  login() {
+    console.log('Login:', this.username, this.password);
+  }
+
+  registrar(){
+    console.log('Registrar:', this.username, this.password);
+  }
+
+  toggleMode() {
+    this.isLogin = !this.isLogin;
+  }
+}
