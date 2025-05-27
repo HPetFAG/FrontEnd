d8bc2976691f00fb3de1cb803a6c6daf872d208a
unkv0wn <cjeanb0@gmail.com>
2025-05-26 21:52:30 -0300

    💄 Criando cards da homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Heart, LucideAngularModule, PawPrint, TrendingDown, TrendingUp } from 'lucide-angular';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './homepage.component.html',
 
 })
 export class HomepageComponent {
-
+  readonly paw = PawPrint
+  readonly trendinup = TrendingUp
+    readonly trendinDown = TrendingDown
+  readonly heart = Heart
 }
