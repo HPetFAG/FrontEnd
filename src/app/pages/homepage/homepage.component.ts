c0d5942a655937c2cac39ed80a232a5c0eb5b3f4
unkv0wn <cjeanb0@gmail.com>
2025-05-29 16:47:20 -0300

    🤡 Ajuste de altura do  donation Card e componentizando dados

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
-import { Cake, CakeSlice, ChevronLeft, ChevronRight, Heart, LucideAngularModule, Map, Mars, PawPrint, Phone, TrendingDown, TrendingUp, User } from 'lucide-angular';
-import { BasicCardComponent } from "../../components/basic-card/basic-card.component";
+import {
+  Cake,
+  CakeSlice,
+  ChevronLeft,
+  ChevronRight,
+  Heart,
+  LucideAngularModule,
+  Map,
+  Mars,
+  PawPrint,
+  Phone,
+  TrendingDown,
+  TrendingUp,
+  User,
+} from 'lucide-angular';
+import { BasicCardComponent } from '../../components/basic-card/basic-card.component';
+import { AdoptionCardComponent } from "../../components/adoption-card/adoption-card.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [LucideAngularModule, BasicCardComponent],
+  imports: [LucideAngularModule, BasicCardComponent, AdoptionCardComponent],
   templateUrl: './homepage.component.html',
-
 })
 export class HomepageComponent {
-  readonly paw = PawPrint
-  readonly trendinup = TrendingUp
-  readonly trendinDown = TrendingDown
-  readonly heart = Heart
-  readonly Cake = Cake
-  readonly mars = Mars
-  readonly map = Map
-  readonly user = User
-  readonly phone = Phone
-  readonly Chevronleft = ChevronLeft
-  readonly Chevronright = ChevronRight
+  readonly paw = PawPrint;
+  readonly trendinup = TrendingUp;
+  readonly trendinDown = TrendingDown;
+  readonly heart = Heart;
+  readonly Chevronleft = ChevronLeft;
+  readonly Chevronright = ChevronRight;
 }
