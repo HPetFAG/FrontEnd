712dfe3fc30b2414d149f5f442193126eda15936
unkv0wn <cjeanb0@gmail.com>
2025-06-04 21:42:30 -0300

    ✨ Adpatando rotas privadas
    rotas protegidas por token do jwt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { ReviewDonationsComponent } from './pages/review-donations/review-donations.component';
 import { AnimalComponent } from './pages/animal/animal.component';
+import { authGuard } from './service/auth/guards/auth.guards';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -11,6 +12,7 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate:[authGuard],
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomepageComponent },
