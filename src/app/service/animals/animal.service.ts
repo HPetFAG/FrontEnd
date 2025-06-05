8b35abf37f57895da9ddeb46e554f93127220f15
unkv0wn <cjeanb0@gmail.com>
2025-06-05 14:56:28 -0300

    👔 Adicionando metodos da entidade animal

---
+++ src/app/service/animals/animal.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../config/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimalService {
+  private apiUrl = `${environment.apiUrl}/animal`;
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  createUser(userData: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, userData);
+  }
+
+  updateUser(id: string, userData: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, userData);
+  }
+
+  deleteUser(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+}
