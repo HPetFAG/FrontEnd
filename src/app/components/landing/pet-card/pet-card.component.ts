dd738f19aa8a7cc48703fba51e442cd1681988f0
unkv0wn <cjeanb0@gmail.com>
2025-06-09 12:34:11 -0300

    🚩 Componetizando card da landing page

--- src/app/components/landing/pet-card/pet-card.component.ts
+++ src/app/components/landing/pet-card/pet-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'PetApresentation',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pet-card.component.html',
 })
 export class PetCardComponent {
-
+  @Input() species: string = ''
+  @Input() localization: string = ''
+  @Input() age: string = ''
+  @Input() size: string = ''
+  @Input() description: string = ''
 }
