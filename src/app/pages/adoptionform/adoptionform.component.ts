07eb88edc476efb1ceb76fe534e571e2c0ba9b1a
unkv0wn <cjeanb0@gmail.com>
2025-06-09 16:11:49 -0300

    👔 Tela de formulario para adocao criada

--- src/app/pages/adoptionform/adoptionform.component.ts
+++ src/app/pages/adoptionform/adoptionform.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule, PawPrint } from 'lucide-angular';
 import { InputComponent } from "../../components/forms/input/input.component";
+import { SelectComponent } from "../../components/forms/select/select.component";
+import { TextareaComponent } from "../../components/forms/textarea/textarea.component";
 
 @Component({
   selector: 'app-adoptionform',
-  imports: [LucideAngularModule, InputComponent],
+  imports: [LucideAngularModule, InputComponent, SelectComponent, TextareaComponent],
   templateUrl: './adoptionform.component.html',
 
 })
