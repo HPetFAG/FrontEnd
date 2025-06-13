8e9d50c4bfb6f466dc91247fccf2d71566ac8b12
unkv0wn <cjeanb0@gmail.com>
2025-06-12 23:17:16 -0300

    👔 Puxando dados do formulario

--- src/app/pages/formsanalysis/formsanalysis.component.ts
+++ src/app/pages/formsanalysis/formsanalysis.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LucideAngularModule, PawPrint } from 'lucide-angular';
 import { Animal } from '../../models/animal.model';
 import { Form } from '../../models/forms.model';
-import { InputComponent } from "../../components/forms/input/input.component";
-import { SelectComponent } from "../../components/forms/select/select.component";
-import { TextareaComponent } from "../../components/forms/textarea/textarea.component";
+import { InputComponent } from '../../components/forms/input/input.component';
+import { SelectComponent } from '../../components/forms/select/select.component';
+import { TextareaComponent } from '../../components/forms/textarea/textarea.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AnimalService } from '../../service/animals/animal.service';
+import { FormService } from '../../service/forms/form.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formsanalysis',
-  imports: [LucideAngularModule, InputComponent, SelectComponent, TextareaComponent],
+  imports: [
+    LucideAngularModule,
+    InputComponent,
+    SelectComponent,
+    TextareaComponent,
+    FormsModule
+  ],
   templateUrl: './formsanalysis.component.html',
 })
-export class FormsanalysisComponent {
+export class FormsanalysisComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private animalService: AnimalService,
+    private formsService: FormService,
+    private route: ActivatedRoute
+  ) {}
+
+  isEditMode = false;
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      this.isEditMode = true;
+      const id = Number(idParam);
+      this.loadAnimal(id);
+    }
+  }
+
   Animal: Animal = {
     id: 0,
     name: '',
@@ -51,4 +79,20 @@ export class FormsanalysisComponent {
   };
 
   readonly paw = PawPrint;
+
+  loadAnimal(id: number): void {
+    this.formsService.getFormById(id).subscribe({
+      next: (animal) => {
+        if (!animal) {
+          console.error('Formulário não encontrado!');
+          return;
+        }
+        this.Forms = animal[0];
+        console.log(animal[0]);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar animal', err);
+      },
+    });
+  }
 }
